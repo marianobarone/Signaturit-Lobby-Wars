3308c1be9f2f5929142adba0586c82f895bf3eb8
marianobarone <marianombarone@gmail.com>
2022-08-01 11:04:28 +0200

    Added unit testing to project

---
+++ Signaturit-Lobby-Wars/UnitTesting/ContractTest.cs
@@ -0,0 +1,143 @@
+﻿using Signaturit_Lobby_Wars.Models.Classes;
+using Signaturit_Lobby_Wars.Services;
+using Xunit;
+using static Signaturit_Lobby_Wars.Helpers.Utils;
+
+namespace Signaturit_Lobby_Wars.UnitTesting
+{
+    public class ContractTest
+    {
+        [Fact]
+        public void GetWinner_ContractAWins()
+        {
+            Contract contractA = new Contract()
+            {
+                Signatures = { SignatureRole.K, SignatureRole.N }
+            };
+            Contract contractB = new Contract()
+            {
+                Signatures = { SignatureRole.N, SignatureRole.N, SignatureRole.V }
+            };
+            Lawsuits winner = new Lawsuits();
+            Contract result = winner.GetWinner(contractA, contractB);
+
+            Assert.Equal(result, contractA);
+        }
+
+        [Fact]
+        public void GetWinner_ContractBWins()
+        {
+            Contract contractA = new Contract()
+            {
+                Signatures = { SignatureRole.K, SignatureRole.N }
+            };
+            Contract contractB = new Contract()
+            {
+                Signatures = { SignatureRole.N, SignatureRole.K, SignatureRole.N }
+            };
+            Lawsuits winner = new Lawsuits();
+            Contract result = winner.GetWinner(contractA, contractB);
+
+            Assert.Equal(result, contractB);
+        }
+
+        [Fact]
+        public void GetWinner_TiedContractSignatures()
+        {
+            Contract contractA = new Contract()
+            {
+                Signatures = { SignatureRole.K, SignatureRole.N }
+            };
+            Contract contractB = new Contract()
+            {
+                Signatures = { SignatureRole.N, SignatureRole.K, SignatureRole.V }
+            };
+            Lawsuits winner = new Lawsuits();
+
+            Action act = () => winner.GetWinner(contractA, contractB);
+            Exception exception = Assert.Throws<Exception>(act);
+
+            Assert.Equal("There is no contract winner. Signatures points of both contract are equals", exception.Message);
+        }
+
+        [Fact]
+        public void GetMinimunSignatureToWin_N()
+        {
+            Contract contractA = new Contract()
+            {
+                Signatures = { SignatureRole.N, SignatureRole.NONE, SignatureRole.V }
+            };
+            Contract contractB = new Contract()
+            {
+                Signatures = { SignatureRole.N, SignatureRole.V, SignatureRole.V }
+            };
+
+            Lawsuits winner = new Lawsuits();
+            SignatureRole result = winner.GetMinimunSignatureToWin(contractA, contractB);
+            SignatureRole mininumExpectedSignature = SignatureRole.N;
+
+            Assert.Equal(result, mininumExpectedSignature);
+        }
+
+        [Fact]
+        public static void GetMinimunSignatureToWin_MoreThanOneEmptySignature()
+        {
+            Contract contractA = new Contract()
+            {
+                Signatures = { SignatureRole.N, SignatureRole.NONE, SignatureRole.NONE }
+            };
+            Contract contractB = new Contract()
+            {
+                Signatures = { SignatureRole.N, SignatureRole.V, SignatureRole.V }
+            };
+
+            Lawsuits winner = new Lawsuits();
+
+            Action act = () => winner.GetMinimunSignatureToWin(contractA, contractB);
+            Exception exception = Assert.Throws<Exception>(act);
+
+            Assert.Equal("GetMinimunSignatureToWin -> Error while looking for the minimun signature to win - There is a contract with more than one empty signature", exception.Message);
+        }
+
+        [Fact]
+        public static void GetMinimunSignatureToWin_SameContractSignatures()
+        {
+            Contract contractA = new Contract()
+            {
+                Signatures = { SignatureRole.K, SignatureRole.NONE, SignatureRole.V }
+            };
+            Contract contractB = new Contract()
+            {
+                Signatures = { SignatureRole.K, SignatureRole.NONE, SignatureRole.V }
+            };
+
+            Lawsuits winner = new Lawsuits();
+
+            Action act = () => winner.GetMinimunSignatureToWin(contractA, contractB);
+            Exception exception = Assert.Throws<Exception>(act);
+
+            Assert.Equal("GetMinimunSignatureToWin -> Error while looking for the minimun signature to win - SearchSignatureToWin -> Error while looking for signature to win - Error, both contracts have the same signatures points", exception.Message);
+        }
+
+        [Fact]
+        public static void GetMinimunSignatureToWin_NoMinimunSignatureFound()
+        {
+            Contract contractA = new Contract()
+            {
+                Signatures = { SignatureRole.K, SignatureRole.K, SignatureRole.V }
+            };
+            Contract contractB = new Contract()
+            {
+                Signatures = { SignatureRole.K, SignatureRole.V, SignatureRole.V }
+            };
+
+            Lawsuits winner = new Lawsuits();
+
+            Action act = () => winner.GetMinimunSignatureToWin(contractA, contractB);
+            Exception exception = Assert.Throws<Exception>(act);
+
+            Assert.Equal("GetMinimunSignatureToWin -> Error while looking for the minimun signature to win - No one of the available signatures it's availabe to win the trial", exception.Message);
+        }
+    }
+}
+
